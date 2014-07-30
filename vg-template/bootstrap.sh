41857dce15feb1e144dd69721989d3be79e58a01
Justin Woo <moomoowoo@gmail.com>
2014-07-29 22:27:48 -0400

    Some more cleanup on vagrant file and bootstrap

--- vg-template/bootstrap.sh
+++ vg-template/bootstrap.sh
@@ -2,7 +2,7 @@
 apt-get install -y git
 apt-get install -y curl
 cd /home/vagrant/
-git clone https://github.com/kimagure/dotfiles
+su vagrant -c 'git clone https://github.com/kimagure/dotfiles'
 cd dotfiles
 su vagrant -c '/home/vagrant/dotfiles/install-deps.sh'
 su vagrant -c '/home/vagrant/dotfiles/bootstrap.sh -f'
