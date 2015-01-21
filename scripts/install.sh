c5ab80e7e38360e2fedff0617194abcbc5d28839
Justin Woo <moomoowoo@gmail.com>
2015-01-21 11:31:37 -0500

    Add janus after dotvim
    Janus makes it dead easy for anyone to learn how to use my setup, so
    might as well use it again.

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,5 +4,7 @@ ln -nsf `pwd`/dotfiles/bash_profile ~/.bash_profile
 ln -nsf `pwd`/dotfiles/bash_aliases ~/.bash_aliases
 ln -nsf `pwd`/dotfiles/bash_prompt ~/.bash_prompt
 ln -nsf `pwd`/dotfiles/tmux.conf ~/.tmux.conf
+ln -nsf `pwd`/dotfiles/vimrc.after ~/.vimrc.after
+ln -nsf `pwd`/dotfiles/gvimrc.after ~/.gvimrc.after
 ln -nsf `pwd`/bin ~/bin
 echo 'symlinks made, check this file for links'
